229b963a8b568bbffe73718597cbeb006e9cc51b
hossein <hossein.tabei@gmail.com>
2023-12-15 16:53:29 +0330

    re-factor

--- backend/src/main/java/demo/application/backend/WebApplication.java
+++ backend/src/main/java/demo/application/backend/WebApplication.java
@@ -1,9 +1,7 @@
 package demo.application.backend;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.SpringVersion;
 
 @SpringBootApplication
 public class WebApplication {
